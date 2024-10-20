40bd65bf28faab04ac5778815174f4236a68c8db
varinmehta <mehtavarin2004@gmail.com>
2024-10-20 08:34:09 +0530

    commit

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const express = require('express')
+const express = require("express");
 
-const PORT = 3000
-const app = express()
+const PORT = 3000;
+const app = express();
 
-app.use(express.static('./public'))
-app.use(express.json())
-app.listen(PORT, () => console.log(`Server listening on ${PORT}`))
+app.use(express.static("./public"));
+app.use(express.json());
+
+const userStore = {};
+app.post("/register", (req, res) => {
+    const { username, password } = req.body;
+    const id = `${Date.now()}`;
+    const user = {
+        id,
+        username,
+        password,
+    };
+    userStore[id] = user;
+    console.log(`registered successfully`, userStore[id]);
+    return res.json({ id });
+});
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
