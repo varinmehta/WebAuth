30530cf7bb7da8ac4c4a46026177e014d03c5664
varinmehta <mehtavarin2004@gmail.com>
2024-10-20 08:50:29 +0530

    commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const { generateRegistrationOptions } = require("@simplewebauthn/server");
 const PORT = 3000;
 const app = express();
 
@@ -7,6 +7,7 @@ app.use(express.static("./public"));
 app.use(express.json());
 
 const userStore = {};
+const challengeStore = {};
 app.post("/register", (req, res) => {
     const { username, password } = req.body;
     const id = `user_${Date.now()}`;
@@ -18,5 +19,20 @@ app.post("/register", (req, res) => {
     userStore[id] = user;
     console.log(`registered successfully`, userStore[id]);
     return res.json({ id });
+});
+
+app.post("/register-challenge", async (req, res) => {
+    const { userId } = req.body;
+    if (!userStore[userId])
+        return res.status(404).json({ error: "user not found!" });
+    const user = userStore[userId];
+    const challengePayload = await generateRegistrationOptions({
+        rpId: "localhost",
+        rpName: "My LocalHost Machine",
+        username: user.username,
+    });
+    challengeStore[userId] = challengePayload.challenge;
+
+    return res.json({ options: challengePayload });
 });
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
